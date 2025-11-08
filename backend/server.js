b30b9de38b93abb106a12b6c50e0321d56656c2f
Rathore-shubham <sr1849587@gmail.com>
2025-11-08 21:38:22 +0530

    user apis and user management

--- backend/server.js
+++ backend/server.js
@@ -215,18 +215,200 @@
 
 
 
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import connectDb from "./config/db.js";
+// // import userRoutes from "./routes/userRoutes.js";
+// // import appointmentRoutes from "./routes/appointmentRoutes.js";
+// // import authRoutes from "./routes/authRoutes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+
+// connectDb();
+
+
+// const userSchema = new mongoose.Schema({
+//   name: String,
+//   email: String
+// })
+
+// const productSchema = new mongoose.Schema({
+//   name: String,
+//   price: Number
+// })
+
+// const orderSchema = new mongoose.Schema({
+//   user: { type: mongoose.Schema.Types.ObjectId, ref:  "User" },
+//   product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+//   quantity: Number,
+// })
+
+// const User = mongoose.model("User", userSchema);
+// const Product = mongoose.model("Product", productSchema);
+// const Order = mongoose.model("Order", orderSchema);
+
+
+// app.post("/seed", async (req, res) => {
+//   await User.deleteMany({});
+//   await Product.deleteMany({});
+//   await Order.deleteMany({});
+
+//   const user1 = await User.create({ name: "Amit", email: "amit@mail.com" });
+//   const user2 = await User.create({ name: "Riya", email: "riya@mail.com" });
+
+//   const prod1 = await Product.create({ name: "Gold Ring", price: 5000 });
+//   const prod2 = await Product.create({ name: "Silver Chain", price: 2000 });
+
+//   await Order.create([
+//     { user: user1._id, product: prod1._id, quantity: 2 },
+//     { user: user2._id, product: prod2._id, quantity: 3 },
+//     { user: user1._id, product: prod2._id, quantity: 1 },
+//   ]);
+
+//   res.json({ success: true, message: "Sample Data Added" });
+// });
+
+
+// app.get("/orders-populate", async (req, res) => {
+//   const orders = await Order.find()
+//     .populate("user", "name email")  
+//     .populate("product", "name price")
+//     .sort({ _id: -1 });
+
+//   res.json({ success: true, data: orders })
+// })
+
+// app.get("/orders-revenue", async (req,res) => {
+//   const stats = await Order.aggregate([
+//     {
+//       $lookup: { //join kr ta he products ko ---> yaha hum order collection ko product collection k sath join kr rhe hain
+//         from: "products", // kis collection se join krvana hai
+//         localField: "product",// current collection(order) me konsa field use hoga join k liye 
+//         foreignField: "_id", // dusri collection me konsa field se match karna hai --> "_id"
+//         as: "productDetails", // result me jo naya field banega uska nam --> "productdetails" 
+//       },
+//     },
+//     { $unwind: "$productDetails" },// unwind -> flatten product array , yani array ka element directly object me aa jata hai
+//     { // add new calculated field totalAMount
+//       $addFields: {
+//         totalAmount: { $multiply: ["$quantity", "$productDetails.price"] }
+//       }
+//     },
+//     { // sum up all total amounts --> total revenue
+//       $group: {
+//         _id: null,
+//         totalRevenue: { $sum: "$totalAmount" },
+//       }
+//     }
+//   ])
+//   res.json({ success: true, data: stats })
+// })
+
+
+
+// app.get("/orders-lookup", async (req, res) => {
+//   const result = await Order.aggregate([
+//     {
+//       $lookup: {
+//         from: "users",
+//         localField: "user",
+//         foreignField: "_id",
+//         as: "userInfo"
+//       },
+//     },
+//     { $unwind: "$userInfo" },
+//     {
+//       $lookup: {
+//         from : "products",
+//         localField: "product",
+//         foreignField: "_id",
+//         as: "productInfo"
+//       }
+//     },
+//     {
+//        $unwind: "$productInfo"
+//     },
+//     {
+//       $project: {
+//         _id: 0,
+//         userName: "$userInfo.name",
+//         productName: "$productInfo.name",
+//         totalAmount: {
+//           $multiply: ["$quantity", "$productInfo.price"]
+//         }
+//       }
+//     }
+//   ])  
+//   res.json({ success: true, data: result})
+// })
+
+
+// app.get("/api", (req, res) => {
+//   const users = [
+//     {
+//       id: 1, name: "Shubham",
+//     },
+//     {
+//       id: 2, name: "Rathore"
+//     }
+//   ]
+
+//   res.json(users)
+// })
+
+
+// app.get("/users", async (req, res) => {
+//   const users = await User.find();
+//   res.json(users)
+// })
+
+// app.post("/create", async (req,res) => {
+//   const newOrder = await Order.create(req.body);
+//   res.status(201).json(newOrder)
+// })
+
+
+// app.put("/order/:id", async (req,res) => {
+//   const updateOrder = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true })
+//   res.status(201).json(updateOrder)
+// })
+
+
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// ?<&$*^@!------------------------------------------------------------------------------------------------------------------------!@^*$&>?
+
+
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDb from "./config/db.js";
-// import userRoutes from "./routes/userRoutes.js";
-// import appointmentRoutes from "./routes/appointmentRoutes.js";
-// import authRoutes from "./routes/authRoutes.js";
+
 
 dotenv.config();
 
-const app = express();
+const app = express()
+
 
 app.use(
   cors({
@@ -242,155 +424,72 @@ connectDb();
 
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema)
+
+
+
+app.post("/api/users", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 })
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching users"})
+  }  
 })
 
-const orderSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref:  "User" },
-  product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-  quantity: Number,
+app.get("/api/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 })
 
-const User = mongoose.model("User", userSchema);
-const Product = mongoose.model("Product", productSchema);
-const Order = mongoose.model("Order", orderSchema);
-
-
-app.post("/seed", async (req, res) => {
-  await User.deleteMany({});
-  await Product.deleteMany({});
-  await Order.deleteMany({});
-
-  const user1 = await User.create({ name: "Amit", email: "amit@mail.com" });
-  const user2 = await User.create({ name: "Riya", email: "riya@mail.com" });
-
-  const prod1 = await Product.create({ name: "Gold Ring", price: 5000 });
-  const prod2 = await Product.create({ name: "Silver Chain", price: 2000 });
-
-  await Order.create([
-    { user: user1._id, product: prod1._id, quantity: 2 },
-    { user: user2._id, product: prod2._id, quantity: 3 },
-    { user: user1._id, product: prod2._id, quantity: 1 },
-  ]);
-
-  res.json({ success: true, message: "Sample Data Added" });
-});
-
-
-app.get("/orders-populate", async (req, res) => {
-  const orders = await Order.find()
-    .populate("user", "name email")  
-    .populate("product", "name price")
-    .sort({ _id: -1 });
-
-  res.json({ success: true, data: orders })
+app.put("/api/users/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedUser)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 })
 
-app.get("/orders-revenue", async (req,res) => {
-  const stats = await Order.aggregate([
-    {
-      $lookup: { //join kr ta he products ko ---> yaha hum order collection ko product collection k sath join kr rhe hain
-        from: "products", // kis collection se join krvana hai
-        localField: "product",// current collection(order) me konsa field use hoga join k liye 
-        foreignField: "_id", // dusri collection me konsa field se match karna hai --> "_id"
-        as: "productDetails", // result me jo naya field banega uska nam --> "productdetails" 
-      },
-    },
-    { $unwind: "$productDetails" },// unwind -> flatten product array , yani array ka element directly object me aa jata hai
-    { // add new calculated field totalAMount
-      $addFields: {
-        totalAmount: { $multiply: ["$quantity", "$productDetails.price"] }
-      }
-    },
-    { // sum up all total amounts --> total revenue
-      $group: {
-        _id: null,
-        totalRevenue: { $sum: "$totalAmount" },
-      }
+
+app.delete("/api/users/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "user deleted succesfully "})
+  } catch (error) {
+    res.status(400).json({ error: error.message })
     }
-  ])
-  res.json({ success: true, data: stats })
 })
-
-
-
-app.get("/orders-lookup", async (req, res) => {
-  const result = await Order.aggregate([
-    {
-      $lookup: {
-        from: "users",
-        localField: "user",
-        foreignField: "_id",
-        as: "userInfo"
-      },
-    },
-    { $unwind: "$userInfo" },
-    {
-      $lookup: {
-        from : "products",
-        localField: "product",
-        foreignField: "_id",
-        as: "productInfo"
-      }
-    },
-    {
-       $unwind: "$productInfo"
-    },
-    {
-      $project: {
-        _id: 0,
-        userName: "$userInfo.name",
-        productName: "$productInfo.name",
-        totalAmount: {
-          $multiply: ["$quantity", "$productInfo.price"]
-        }
-      }
-    }
-  ])  
-  res.json({ success: true, data: result})
-})
-
-
-app.get("/api", (req, res) => {
-  const users = [
-    {
-      id: 1, name: "Shubham",
-    },
-    {
-      id: 2, name: "Rathore"
-    }
-  ]
-
-  res.json(users)
-})
-
-
-app.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.json(users)
-})
-
-app.post("/create", async (req,res) => {
-  const newOrder = await Order.create(req.body);
-  res.status(201).json(newOrder)
-})
-
-
-app.put("/order/:id", async (req,res) => {
-  const updateOrder = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true })
-  res.status(201).json(updateOrder)
-})
-
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,115 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+// import Dashboard from './pages/Dashboard';
 
-function App(){
+import { useEffect, useState } from "react";
+
+// function App(){
+//   return (
+//     <BrowserRouter>
+//       <nav style={{ padding: 12 }}>
+//         <Link to="/">Home</Link> | <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
+//       </nav>
+//       <Routes>
+//         <Route path="/" element={<Dashboard/>} />
+//         <Route path="/login" element={<Login/>} />
+//         <Route path="/register" element={<Register/>} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+// export default App;
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(5);
+
+
+  useEffect(() => {
+    const dummy = [
+      { _id: 1, name: "Aarav", email: "aarav@mail.com" },
+      { _id: 2, name: "Vihaan", email: "vihaan@mail.com" },
+      { _id: 3, name: "Reyansh", email: "rey@mail.com" },
+      { _id: 4, name: "Advik", email: "advik@mail.com" },
+      { _id: 5, name: "Kabir", email: "kabir@mail.com" },
+      { _id: 6, name: "Ishaan", email: "ishaan@mail.com" },
+      { _id: 7, name: "Ayaan", email: "ayaan@mail.com" },
+      { _id: 8, name: "Krishna", email: "krish@mail.com" },
+      { _id: 9, name: "Rudra", email: "rudra@mail.com" },
+    ];
+    setUsers(dummy)
+  }, []);
+
+
+  const filteredUsers = users
+    .filter((u) => u.name.toLowerCase().includes(search.toLowerCase()))
+    .sort((a,b) => 
+      sortOrder === "asc"
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name)
+    )
+
+  const indexOfLast = currentPage * usersPerPage;
+  const indexOfFirst = indexOfLast - usersPerPage;
+  const currentUsers = filteredUsers.slice(indexOfFirst, indexOfLast);
+  const totalPages = Math.ceil(filteredUsers.length / usersPerPage);
+
+  const handlePageChange = (page) => setCurrentPage(page)
+
   return (
-    <BrowserRouter>
-      <nav style={{ padding: 12 }}>
-        <Link to="/">Home</Link> | <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Dashboard/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h2>User management</h2>
+      <input
+        type="text"
+        placeholder="search users..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      {/* Sort */}
+      <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+        <option value="asc">Sort: A - Z </option>
+        <option value="desc">Sort: Z - A </option>
+      </select>
+
+      {/* Table */}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+            {currentUsers.map((user) => {
+              <tr>
+                <td>
+                  {user.name}
+                </td>
+                <td>
+                  {user.email}
+                </td>
+              </tr>
+            })}
+        </tbody>
+      </table>
+
+      {/* pagination */}
+      <div>
+        {
+          Array.from({ length: totalPages }, (_, i) => (
+            <button key={i} onClick={() => handlePageChange(i + 1)}>
+              {i + 1}
+            </button>
+          ))
+        }
+      </div>
+    </div>
   );
-}
+};
+
 export default App;
